9eba8e27060c6cb51e7ca232e6f85eba004bfabe
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-03-03 17:54:28 +0100

    [CI4]First commit

---
+++ app/Controllers/Products.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers;
+
+class Products extends BaseController{
+
+  public function index(){
+    echo "<h1>This is the index method in the Products controller.</h1>";  
+  }
+}
