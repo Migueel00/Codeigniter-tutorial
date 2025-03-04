096761a0c345dd03d2a0f2284723bec95486f21b
Migueel00 <rojassoriamiguelangel95@gmail.com>
2025-03-04 22:50:56 +0100

    [CI4][ADD]Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,13 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/products', 'Products::index');
+$routes->get('/products/([0-9]{2})', 'Products::show/$1'); // Filter for numbers
+$routes->get('/products/(:alpha)/(:num)', 'Products::cat/$1/$2');
+
+$routes->view('productsList/(:alpha)', 'list_products'); // This is a view route
+
+$routes->group('admin', static function ($routes) { // this is a group route it will add /admin to the routes in the group 
+    $routes->get('products', 'Admin\Products::index');
+    $routes->get('products/(:num)', 'Admin\Products::show/$1');
+    $routes->get('products/(:alpha)/(:num)', 'Admin\Products::cat/$1/$2');
+});
--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -5,6 +5,17 @@ namespace App\Controllers;
 class Products extends BaseController{
 
   public function index(){
-    echo "<h1>This is the index method in the Products controller.</h1>";  
+    echo "<h1>This is the index method in the Products controller.</h1>";
+    print_r($this->session);  
   }
+
+  public function show($id) : string{
+    return "<h1>Details of the product $id </h1>";
+  }
+
+  public function cat($categoria, $id) : string{
+    return "<h2>Categoria: $categoria <br> Producto: $id </h2>";
+  }
+
+
 }
